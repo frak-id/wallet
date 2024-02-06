28dc8bcd6fcae5b4a7f295701c6db6cca0a7b42a
Rodolphe Stoclin <srodolphe@gmail.com>
2024-02-06 16:01:45 +0100

    🚚 Redirect on wallet page by default

--- packages/wallet/src/app/page.tsx
+++ packages/wallet/src/app/page.tsx
@@ -1,5 +1,10 @@
-import { WalletHomePage } from "@/module/wallet/component/Home";
+import { getSession } from "@/context/session/action/session";
+import { redirect } from "next/navigation";
 
 export default async function HomePage() {
-    return <WalletHomePage />;
+    // Check if a user is logged in or not
+    const currentSession = await getSession();
+
+    // If we have a current session, redirect to the wallet
+    currentSession && redirect("/wallet");
 }
