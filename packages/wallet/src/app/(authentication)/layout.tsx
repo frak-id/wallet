c0e5fdaf91d891d75444465df307aa5908be46dc
Rodolphe Stoclin <srodolphe@gmail.com>
2024-02-22 17:15:32 +0100

    🐛 Remove layout redirect

--- packages/wallet/src/app/(authentication)/layout.tsx
+++ packages/wallet/src/app/(authentication)/layout.tsx
@@ -1,7 +1,5 @@
-import { getSession } from "@/context/session/action/session";
 import { LastAuthenticationsProvider } from "@/module/authentication/providers/LastAuthentication";
 import { Header } from "@/module/common/component/Header";
-import { redirect } from "next/navigation";
 import type { ReactNode } from "react";
 
 export default async function AuthenticationLayout({
@@ -9,13 +7,6 @@ export default async function AuthenticationLayout({
 }: Readonly<{
     children: ReactNode;
 }>) {
-    // Check if a user is logged in or not
-    const session = await getSession();
-
-    // If we don't have a session, redirect to register
-    if (session) {
-        redirect("/wallet");
-    }
     return (
         <LastAuthenticationsProvider>
             <Header navigation={false} />
