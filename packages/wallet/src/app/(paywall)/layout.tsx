75e78ef546632dd6fc7ce7dae75a1823b526f1d4
Rodolphe Stoclin <srodolphe@gmail.com>
2024-02-22 15:43:49 +0100

    🐛 Fix redirects

--- packages/wallet/src/app/(paywall)/layout.tsx
+++ packages/wallet/src/app/(paywall)/layout.tsx
@@ -1,7 +1,4 @@
-import { getSession } from "@/context/session/action/session";
 import { Header } from "@/module/common/component/Header";
-import { WalletProvider } from "@/module/wallet/provider/WalletProvider";
-import { redirect } from "next/navigation";
 import type { ReactNode } from "react";
 
 export default async function RestrictedLayout({
@@ -9,17 +6,10 @@ export default async function RestrictedLayout({
 }: Readonly<{
     children: ReactNode;
 }>) {
-    // Check if a user is logged in or not
-    const session = await getSession();
-
-    // If we don't have a session, redirect to register
-    if (!session) {
-        redirect("/register");
-    }
     return (
-        <WalletProvider session={session}>
-            <Header navigation={false} authenticated={true} />
+        <>
+            <Header navigation={false} />
             {children}
-        </WalletProvider>
+        </>
     );
 }
