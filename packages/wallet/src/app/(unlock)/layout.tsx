201b6a41188c3150f7fcb838c739bc9ea197448c
Rodolphe Stoclin <srodolphe@gmail.com>
2024-04-09 18:01:45 +0200

    ♻️ Too much getSession, add middleware to handle redirections

--- packages/wallet/src/app/(unlock)/layout.tsx
+++ packages/wallet/src/app/(unlock)/layout.tsx
@@ -1,7 +1,5 @@
-import { getSession } from "@/context/session/action/session";
 import { Header } from "@/module/common/component/Header";
 import { WalletProvider } from "@/module/wallet/provider/WalletProvider";
-import { redirect } from "next/navigation";
 import type { ReactNode } from "react";
 
 export default async function RestrictedLayout({
@@ -9,15 +7,8 @@ export default async function RestrictedLayout({
 }: Readonly<{
     children: ReactNode;
 }>) {
-    // Check if a user is logged in or not
-    const session = await getSession();
-
-    // If we don't have a session, redirect to register
-    if (!session) {
-        redirect("/register");
-    }
     return (
-        <WalletProvider session={session}>
+        <WalletProvider>
             <Header navigation={false} authenticated={true} />
             {children}
         </WalletProvider>
