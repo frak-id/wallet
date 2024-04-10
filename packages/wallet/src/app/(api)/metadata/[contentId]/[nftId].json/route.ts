dd540348dea0c552851b029879d22e5e098eb728
KONFeature <quentin@nivelais.com>
2024-04-10 12:39:37 +0200

    ✨ Add more nft metadata

--- packages/wallet/src/app/(api)/metadata/[contentId]/[nftId].json/route.ts
+++ packages/wallet/src/app/(api)/metadata/[contentId]/[nftId].json/route.ts
@@ -1,5 +1,34 @@
 import { type NextRequest, NextResponse } from "next/server";
 
+type ContentNames = "le-monde" | "equipe" | "wired";
+
+const rangePerContent: Record<ContentNames, [number, number]> = {
+    "le-monde": [1, 3],
+    wired: [1, 5],
+    equipe: [1, 4],
+};
+
+const contentIdToName: Record<number, ContentNames> = {
+    0: "le-monde",
+    1: "equipe",
+    2: "wired",
+};
+
+function getImageForId({
+    contentName,
+    nftId,
+}: { contentName: ContentNames; nftId: number }) {
+    // Get the content name
+    if (!contentName) return "https://placehold.co/400";
+
+    // Get the range for this content
+    const [min, max] = rangePerContent[contentName];
+    if (!(min && max)) return "https://placehold.co/400";
+    // Get an image number in the range from the nft id, the range is inclusive
+    const imageNumber = (nftId % (max - min + 1)) + min;
+    return `https://poc-wallet.frak.id/images/nft/${contentName}-${imageNumber}.webp`;
+}
+
 /**
  * Get the metadata for a specific NFT
  * @param _request
@@ -10,11 +39,19 @@ export function GET(
     _request: NextRequest,
     { params }: { params: { contentId: string; nftId: string } }
 ) {
+    // Get the content name
+    const contentName = contentIdToName[Number.parseInt(params.contentId)];
+
     // TODO: Define the metadata structure we need and we want
     return NextResponse.json({
-        name: "TestToken",
+        name: `${contentName} NFT #${params.nftId}`,
         description: "Super token description",
-        image: "https://placehold.co/400",
-        ...params,
+        image: getImageForId({
+            contentName,
+            nftId: Number.parseInt(params.nftId),
+        }),
+        // Content related
+        contentId: params.contentId,
+        contentType: "news",
     });
 }
