13a7818eb5238c5f1f35d9370743e59f7af546e8
Rodolphe Stoclin <srodolphe@gmail.com>
2024-04-11 15:39:33 +0200

    🔥 Remove unused slick css

--- packages/wallet/src/module/common/component/Slick/index.tsx
+++ packages/wallet/src/module/common/component/Slick/index.tsx
@@ -4,7 +4,6 @@ import { useCommunityTokenMetadata } from "@/module/community-token/hooks/useCom
 import type { CommunityTokenBalance } from "@/types/CommunityTokenBalances";
 import { useMemo } from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import styles from "./index.module.css";
 
