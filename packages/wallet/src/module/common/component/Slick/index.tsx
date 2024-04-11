0991d564fbc0a51d8b9493b2cfa98b592a2a2554
Rodolphe Stoclin <srodolphe@gmail.com>
2024-04-11 14:22:33 +0200

    💄 Use Slick slider

--- packages/wallet/src/module/common/component/Slick/index.tsx
+++ packages/wallet/src/module/common/component/Slick/index.tsx
@@ -28,7 +28,6 @@ export function Slick({ slides = [] }: { slides: CommunityTokenBalance[] }) {
 
 function NftSlide({ nft }: { nft: CommunityTokenBalance }) {
     const { data: metadata } = useCommunityTokenMetadata({
-        tokenAddress: nft.contractAddress,
         id: nft.tokenId,
     });
     const imageUrl = useMemo(
--- packages/wallet/src/module/community-token/component/SwiperNfts/index.tsx
+++ packages/wallet/src/module/community-token/component/SwiperNfts/index.tsx
@@ -1,9 +1,8 @@
-import { Swiper } from "@/module/common/component/Swiper";
+import { Slick } from "@/module/common/component/Slick";
+// import { Swiper } from "@/module/common/component/Swiper";
 import { useCommunityTokens } from "@/module/community-token/hooks/useCommunityTokens";
-// import { Slick } from "@/module/common/component/Slick";
 
 export function SwiperNfts() {
     const { data: userTokens } = useCommunityTokens();
-
-    return <Swiper slides={userTokens ?? []} />;
+    return <Slick slides={userTokens ?? []} />;
 }
