a02303a04978f408e4fcbb0ab6d20c7bdc644e65
KONFeature <quentin@nivelais.com>
2024-05-02 20:44:44 +0200

    ✨ Add get pending reward action

---
+++ packages/wallet/src/context/referral/action/pendingReferral.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { addresses } from "@/context/common/blockchain/addresses";
+import { frakChainPocClient } from "@/context/common/blockchain/provider";
+import { nexusDiscoverCampaignAbi } from "@/context/referral/abi/campaign-abis";
+import { type Address, formatEther } from "viem";
+import { readContract } from "viem/actions";
+
+/**
+ * Tell that a user has been referred by another user
+ * @param user
+ * @param referrer
+ */
+export async function getPendingWalletReferralReward({
+    user,
+}: { user: Address }) {
+    const pendingAmount = await readContract(frakChainPocClient, {
+        address: addresses.nexusDiscoverCampaign,
+        abi: nexusDiscoverCampaignAbi,
+        functionName: "getPendingAmount",
+        args: [user],
+    });
+    if (!pendingAmount) {
+        return undefined;
+    }
+    return {
+        rFrkPendingRaw: pendingAmount,
+        rFrkPendingFormatted: formatEther(pendingAmount),
+    };
+}
