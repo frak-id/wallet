a3ba5e1720a5c73be68c67a406e32719f5fdb924
Rodolphe Stoclin <srodolphe@gmail.com>
2024-05-15 17:06:30 +0200

    ✨ If no referrer, use frak labs governance

--- packages/wallet/src/module/referral/hook/useTriggerReferral.ts
+++ packages/wallet/src/module/referral/hook/useTriggerReferral.ts
@@ -7,7 +7,13 @@ import { useCallback } from "react";
 import type { Hex } from "viem";
 
 /**
- * Small hook used to perform redirection in the paywall context
+ * Wallet governance address
+ */
+const FRAK_WALLET_GOVERNANCE: Hex =
+    "0xB5cbdba1Db3618a68921e7c00e38617c9AfDf220";
+
+/**
+ * Hook to trigger the referral
  */
 export function useTriggerReferral() {
     /**
@@ -16,12 +22,22 @@ export function useTriggerReferral() {
     const [referralHistory, setReferralHistory] = useAtom(referralHistoryAtom);
 
     /**
-     * Trigger the referral
+     * Referral from Frak when the user is not referred
      */
-    const triggerReferral = useCallback(
-        async (wallet: WebAuthNWallet) => {
-            if (!referralHistory) return;
+    const referralFromFrak = useCallback(
+        async (wallet: WebAuthNWallet) =>
+            await setUserReferred({
+                user: wallet.address,
+                referrer: FRAK_WALLET_GOVERNANCE,
+            }),
+        []
+    );
 
+    /**
+     * Referral from the last referred user
+     */
+    const referralFromUser = useCallback(
+        async (wallet: WebAuthNWallet) => {
             // Set the user referred
             await setUserReferred({
                 user: wallet.address,
@@ -38,14 +54,27 @@ export function useTriggerReferral() {
                     contentId: contentId as Hex,
                 });
             }
-
-            // Reset referral history
-            setReferralHistory({
-                contents: {},
-                lastReferrer: "0x00",
-            });
         },
-        [referralHistory, setReferralHistory]
+        [referralHistory]
+    );
+
+    /**
+     * Trigger the referral
+     */
+    const triggerReferral = useCallback(
+        async (wallet: WebAuthNWallet) => {
+            const referralMethod = Object.keys(referralHistory.contents).length
+                ? referralFromUser
+                : referralFromFrak;
+            await referralMethod(wallet);
+            setReferralHistory({ contents: {}, lastReferrer: "0x00" });
+        },
+        [
+            referralHistory,
+            referralFromFrak,
+            referralFromUser,
+            setReferralHistory,
+        ]
     );
 
     return { triggerReferral };
