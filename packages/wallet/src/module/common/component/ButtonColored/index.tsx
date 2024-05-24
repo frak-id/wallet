9c5a6d54d940942e110ca3765bc7295a316e60c1
Rodolphe Stoclin <srodolphe@gmail.com>
2024-05-24 17:58:57 +0200

    🚧 Start Membrs flow

---
+++ packages/wallet/src/module/common/component/ButtonColored/index.tsx
@@ -0,0 +1,32 @@
+import { ButtonRipple } from "@/module/common/component/ButtonRipple";
+import type { PropsWithChildren } from "react";
+import styles from "./index.module.css";
+
+type ButtonRippleSmallProps = {
+    type?: "button" | "submit";
+    onClick?: () => void;
+    disabled?: boolean;
+    className?: string;
+    isLoading?: boolean;
+};
+
+export function ButtonColored({
+    children,
+    type = "button",
+    disabled,
+    onClick,
+    className = "",
+    isLoading,
+}: PropsWithChildren<ButtonRippleSmallProps>) {
+    return (
+        <ButtonRipple
+            type={type}
+            disabled={disabled}
+            onClick={onClick}
+            className={`${styles.buttonRippleSmall} ${className}`}
+            isLoading={isLoading}
+        >
+            {children}
+        </ButtonRipple>
+    );
+}
