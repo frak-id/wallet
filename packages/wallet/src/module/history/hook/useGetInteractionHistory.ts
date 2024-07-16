fcb910a65a6f758a15e40debf2a662f4447b78c0
Rodolphe Stoclin <srodolphe@gmail.com>
2024-07-16 15:42:38 +0200

    💄 Improve interaction history page

--- packages/wallet/src/module/history/hook/useGetInteractionHistory.ts
+++ packages/wallet/src/module/history/hook/useGetInteractionHistory.ts
@@ -1,8 +1,6 @@
 "use client";
 
-import { dexieDb } from "@/context/common/dexie/dexieDb";
 import { getInteractionHistory } from "@/context/history/action/interactionHistory";
-import type { InteractionHistoryWithFrontData } from "@/types/InteractionHistory";
 import { useQuery } from "@tanstack/react-query";
 import { useAccount } from "wagmi";
 
@@ -14,28 +12,9 @@ export function useGetInteractionHistory() {
     const { data: history } = useQuery({
         queryKey: ["history", "interaction", address],
         queryFn: async () => {
-            const rawHistory = await getInteractionHistory({
+            return await getInteractionHistory({
                 account: address ?? "0x",
             });
-
-            // Fetch every frontend data we have in the dexie db
-            const articleInfos = await dexieDb.articleInfo.toArray();
-
-            // Map every interaction with the front data
-            return rawHistory.map((item) => {
-                // Try to find it inside our frontend data
-                const articleInfo = articleInfos.find(
-                    (info) => BigInt(info.contentId) === BigInt(item.contentId)
-                );
-
-                return {
-                    ...item,
-                    articleUrl: articleInfo?.articleUrl,
-                    articleTitle: articleInfo?.articleTitle,
-                    contentTitle: articleInfo?.contentTitle,
-                    provider: articleInfo?.provider,
-                };
-            }) as InteractionHistoryWithFrontData[];
         },
         enabled: !!address,
     });
