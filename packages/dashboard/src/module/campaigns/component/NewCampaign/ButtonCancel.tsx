9f6f3edb7790911b60da26e96f6876440de50089
Rodolphe Stoclin <srodolphe@gmail.com>
2024-08-27 10:38:09 +0200

    💄 Show a confirmation modal before canceling a campaign

--- packages/dashboard/src/module/campaigns/component/NewCampaign/ButtonCancel.tsx
+++ packages/dashboard/src/module/campaigns/component/NewCampaign/ButtonCancel.tsx
@@ -1,25 +1,49 @@
 import { campaignResetAtom } from "@/module/campaigns/atoms/campaign";
+import { AlertDialog } from "@/module/common/component/AlertDialog";
 import { Button } from "@module/component/Button";
 import { useSetAtom } from "jotai";
 import { X } from "lucide-react";
+import { useState } from "react";
 
 export function ButtonCancel({
     onClick,
     disabled,
 }: { onClick: () => void; disabled?: boolean }) {
     const campaignReset = useSetAtom(campaignResetAtom);
+    const [open, setOpen] = useState(false);
 
     return (
-        <Button
-            variant={"outline"}
-            leftIcon={<X size={20} />}
-            onClick={() => {
-                campaignReset();
-                onClick?.();
-            }}
-            disabled={disabled}
-        >
-            Cancel
-        </Button>
+        <AlertDialog
+            open={open}
+            onOpenChange={setOpen}
+            title={"Cancel campaign"}
+            buttonElement={
+                <Button
+                    variant={"outline"}
+                    leftIcon={<X size={20} />}
+                    disabled={disabled}
+                >
+                    Cancel
+                </Button>
+            }
+            description={
+                <>
+                    Are you sure you want to cancel the campaign ?<br />
+                    Form will be reset to its initial state.
+                </>
+            }
+            action={
+                <Button
+                    variant={"danger"}
+                    onClick={() => {
+                        campaignReset();
+                        onClick?.();
+                        setOpen(false);
+                    }}
+                >
+                    Cancel campaign
+                </Button>
+            }
+        />
     );
 }
