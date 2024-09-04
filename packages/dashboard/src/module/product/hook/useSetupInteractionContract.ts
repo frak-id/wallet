67805d78864beec9a68eb180f5b06dc68298e0b8
KONFeature <quentin@nivelais.com>
2024-09-04 17:39:05 +0200

    🚸 Single tx for interaction deployment + roles addition

--- packages/dashboard/src/module/product/hook/useSetupInteractionContract.ts
+++ packages/dashboard/src/module/product/hook/useSetupInteractionContract.ts
@@ -1,19 +1,25 @@
 import { interactionValidatorRoles } from "@/context/blockchain/roles";
 import { getManagedValidatorPublicKey } from "@/context/product/action/getValidator";
 import { useWaitForTxAndInvalidateQueries } from "@/module/common/utils/useWaitForTxAndInvalidateQueries";
-import { useSendTransactionAction } from "@frak-labs/nexus-sdk/react";
+import type { SendTransactionModalStepType } from "@frak-labs/nexus-sdk/core";
+import {
+    useSendTransactionAction,
+    useWalletStatus,
+} from "@frak-labs/nexus-sdk/react";
 import { currentViemClient } from "@frak-labs/nexus-wallet/src/context/blockchain/provider";
 import { productInteractionManagerAbi } from "@frak-labs/shared/context/blockchain/abis/frak-interaction-abis";
 import { addresses } from "@frak-labs/shared/context/blockchain/addresses";
 import { useMutation } from "@tanstack/react-query";
 import { type Hex, encodeFunctionData } from "viem";
-import { readContract } from "viem/actions";
+import { generatePrivateKey } from "viem/accounts";
+import { simulateContract } from "viem/actions";
 
 /**
  * Hook used to setup the interaction contract on the given productId
  * todo: Should have a simulation and a precise deployed address to avoid the need of 2 tw, and instead just send one batched tx
  */
 export function useSetupInteractionContract() {
+    const { data: walletStatus } = useWalletStatus();
     const { mutateAsync: sendTransaction } = useSendTransactionAction();
     const waitForTxAndInvalidateQueries = useWaitForTxAndInvalidateQueries();
 
@@ -23,16 +29,62 @@ export function useSetupInteractionContract() {
             directAllowValidator,
             productId,
         }: { productId: Hex; directAllowValidator: boolean }) => {
-            // Send the deploy interaction call
-            const { hash: interactionDeploymentHash } = await sendTransaction({
-                tx: {
+            // early exit if user not logged in
+            if (walletStatus?.key !== "connected") return;
+
+            // Generate a random bytes32
+            const randomBytes32 = generatePrivateKey();
+
+            // First tx to deploy the interaction contract
+            const tx: SendTransactionModalStepType["params"]["tx"] = [
+                {
                     to: addresses.productInteractionManager,
                     data: encodeFunctionData({
                         abi: productInteractionManagerAbi,
                         functionName: "deployInteractionContract",
-                        args: [BigInt(productId)],
+                        args: [BigInt(productId), randomBytes32],
                     }),
                 },
+            ];
+
+            // If we directly want to allow the managed validator, add another tx
+            if (directAllowValidator) {
+                // Predicate final address
+                const { result: predictedInteractionAddress } =
+                    await simulateContract(currentViemClient, {
+                        account: walletStatus.wallet,
+                        address: addresses.productInteractionManager,
+                        abi: productInteractionManagerAbi,
+                        functionName: "deployInteractionContract",
+                        args: [BigInt(productId), randomBytes32],
+                    });
+
+                // Get the manager validator address
+                const { productPubKey } = await getManagedValidatorPublicKey({
+                    productId: productId,
+                });
+                if (!(productPubKey && predictedInteractionAddress)) {
+                    console.log(
+                        "Error getting the product pub key or the predicted interaction address",
+                        { productPubKey, predictedInteractionAddress }
+                    );
+                    return;
+                }
+
+                // Add the second tx to allow the managed validator
+                tx.push({
+                    to: predictedInteractionAddress,
+                    data: encodeFunctionData({
+                        abi: productInteractionManagerAbi,
+                        functionName: "grantRoles",
+                        args: [productPubKey, interactionValidatorRoles],
+                    }),
+                });
+            }
+
+            // Send the deploy interaction call
+            const { hash } = await sendTransaction({
+                tx,
                 metadata: {
                     header: {
                         title: "Deploy interaction handler",
@@ -41,54 +93,10 @@ export function useSetupInteractionContract() {
                         "Deploying user interactions handler for your product",
                 },
             });
-            if (!directAllowValidator) {
-                // Wait for the tx to be done, and invalidate product and campaigns related queries
-                await waitForTxAndInvalidateQueries({
-                    hash: interactionDeploymentHash,
-                    queryKey: ["product"],
-                });
-                return;
-            }
-
-            // Get the manager validator address
-            const { productPubKey } = await getManagedValidatorPublicKey({
-                productId: productId,
-            });
-            if (!productPubKey) {
-                return;
-            }
-            // Get the interaction contract address
-            const interactionContractAddress = await readContract(
-                currentViemClient,
-                {
-                    address: addresses.productInteractionManager,
-                    abi: productInteractionManagerAbi,
-                    functionName: "getInteractionContract",
-                    args: [BigInt(productId)],
-                }
-            );
-            // Allow it
-            const { hash: validatorAllowanceHash } = await sendTransaction({
-                tx: {
-                    to: interactionContractAddress,
-                    data: encodeFunctionData({
-                        abi: productInteractionManagerAbi,
-                        functionName: "grantRoles",
-                        args: [productPubKey, interactionValidatorRoles],
-                    }),
-                },
-                metadata: {
-                    header: {
-                        title: "Allow managed validator",
-                    },
-                    context:
-                        "Allowing the managed validator to approve interaction on your product",
-                },
-            });
 
             // Wait for the tx to be done, and invalidate product and campaigns related queries
             await waitForTxAndInvalidateQueries({
-                hash: validatorAllowanceHash,
+                hash,
                 queryKey: ["product"],
             });
         },
--- packages/shared/context/blockchain/addresses.ts
+++ packages/shared/context/blockchain/addresses.ts
@@ -7,7 +7,7 @@ export const addresses = {
     referralRegistry: "0xcf5855d9825578199969919F1696b80388111403",
     productAdministratorRegistry: "0x62254d732C078BF0484EA7dBd61f7F620184F95e",
     // Interactions
-    productInteractionManager: "0x5c449C1777Fa729C4136DDF81585FDd7512Ae8bb",
+    productInteractionManager: "0xC9d1BAB1B9A07c11AB0C264B13AFfD500DD4c2ee",
     // Tokens
     mUsdToken: "0x56039fa1a804F614eBD714139F29a3ff4DB57ad6",
     // Interactions
