6f2b3907181932e23644a55cd88ca4dd91cf86b7
Rodolphe Stoclin <srodolphe@gmail.com>
2024-09-04 11:35:08 +0200

    💄 Update balance and add funds button

--- packages/dashboard/src/module/campaigns/component/CampaignDetails/CampaignBalance.tsx
+++ packages/dashboard/src/module/campaigns/component/CampaignDetails/CampaignBalance.tsx
@@ -1,25 +1,10 @@
 import { getOnChainCampaignsDetails } from "@/context/campaigns/action/getDetails";
 import { addCampaignFund } from "@/context/campaigns/action/reload";
-import { Row } from "@/module/common/component/Row";
 import { Title } from "@/module/common/component/Title";
-import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormMessage,
-} from "@/module/forms/Form";
 import { Button } from "@module/component/Button";
-import { Input } from "@module/component/forms/Input";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { sleep } from "radash";
-import { useEffect } from "react";
-import { useForm } from "react-hook-form";
 import { type Address, formatEther } from "viem";
-
-type FormBalance = {
-    balance: number;
-};
 
 /**
  * Display the campaign balance
@@ -52,26 +37,7 @@ export function CampaignBalance({
         },
     });
 
-    const form = useForm<FormBalance>({
-        defaultValues: {
-            balance: 0,
-        },
-    });
-
-    useEffect(() => {
-        if (!onChainInfos) return;
-        form.setValue(
-            "balance",
-            Number(formatEther(BigInt(onChainInfos.balance)))
-        );
-    }, [onChainInfos, form]);
-
-    function onSubmit(formData: FormBalance) {
-        console.log(formData);
-        addFundRequest();
-    }
-
-    if (isLoading) {
+    if (isLoading || !onChainInfos) {
         return null;
     }
 
@@ -80,43 +46,20 @@ export function CampaignBalance({
             <Title as={"h3"} size={"small"}>
                 Balance
             </Title>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)}>
-                    <FormField
-                        control={form.control}
-                        name="balance"
-                        rules={{
-                            validate: {
-                                required: (value) =>
-                                    value > 0 || "Invalid balance",
-                            },
-                        }}
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormControl>
-                                    <Row>
-                                        <Input
-                                            length={"medium"}
-                                            placeholder={"Domain Name...."}
-                                            disabled={isAddingFund}
-                                            {...field}
-                                        />
-                                        <Button
-                                            type={"submit"}
-                                            variant={"submit"}
-                                            isLoading={isAddingFund}
-                                            disabled={isAddingFund}
-                                        >
-                                            Add Funds
-                                        </Button>
-                                    </Row>
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                </form>
-            </Form>
+            <p>
+                Your current balance:{" "}
+                {formatEther(BigInt(onChainInfos.balance))}
+            </p>
+            <p>
+                <Button
+                    variant={"submit"}
+                    isLoading={isAddingFund}
+                    disabled={isAddingFund}
+                    onClick={() => addFundRequest()}
+                >
+                    Add Funds
+                </Button>
+            </p>
         </>
     );
 }
