abbffcf8d9e856271b10ce210ff1703b1a882aa6
Rodolphe Stoclin <srodolphe@gmail.com>
2024-09-06 14:42:57 +0200

    💄 Show Lottie only if wallet is connected

--- example/news-interactions/src/module/common/component/Lottie/index.tsx
+++ example/news-interactions/src/module/common/component/Lottie/index.tsx
@@ -1,10 +1,18 @@
 "use client";
 
+import { useWalletStatus } from "@frak-labs/nexus-sdk/react";
 import { Player } from "@lottiefiles/react-lottie-player";
 import { Link } from "next-view-transitions";
 import lottie from "./assets/lottie.json";
 
 export function Lottie({ className }: { className?: string }) {
+    // Get the wallet status
+    const { data: walletStatus } = useWalletStatus();
+
+    if (walletStatus?.key !== "connected") {
+        return null;
+    }
+
     return (
         <div className={className}>
             <Link href={process.env.NEXUS_WALLET_URL as string}>
