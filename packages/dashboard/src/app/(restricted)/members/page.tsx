690717c31633934b1901c156db3b8eee786d0203
Rodolphe Stoclin <srodolphe@gmail.com>
2024-09-13 16:03:52 +0200

    ✨ Add send push button

--- packages/dashboard/src/app/(restricted)/members/page.tsx
+++ packages/dashboard/src/app/(restricted)/members/page.tsx
@@ -1,5 +1,6 @@
 import { Breadcrumb } from "@/module/common/component/Breadcrumb";
 import { Head } from "@/module/common/component/Head";
+import { ButtonSendPush } from "@/module/members/component/ButtonSendPush";
 import { TableMembers } from "@/module/members/component/TableMembers";
 
 export default function MembersListPage() {
@@ -8,6 +9,7 @@ export default function MembersListPage() {
             <Head
                 title={{ content: "Members" }}
                 leftSection={<Breadcrumb current={"Members List"} />}
+                rightSection={<ButtonSendPush />}
             />
             <TableMembers />
         </>
