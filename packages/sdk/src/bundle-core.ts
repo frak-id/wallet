7560d5031afd8caa22824b59e3320e3181c4ebae
Rodolphe Stoclin <srodolphe@gmail.com>
2024-09-18 18:01:35 +0200

    🚧 Bundle SDK core into one big bundle and add a test page

---
+++ packages/sdk/src/bundle-core.ts
@@ -0,0 +1,3 @@
+export * from "./core/index";
+export * from "./core/actions/index";
+export * from "./core/interactions/index";
---
+++ packages/sdk/tsup.config.bundle.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    // All of our entry-points
+    entry: ["src/bundle-core.ts"],
+    outDir: "dist-bundle",
+    // No external dependencies for these libraries (will be bundled)
+    noExternal: ["async-lz-string", "js-sha256", "viem"],
+    // Format waited
+    format: ["iife"],
+    // Code splitting and stuff
+    clean: true,
+    splitting: false,
+    minify: true,
+    // Expose NexusSDK in global namespace window.NexusSDK
+    globalName: "NexusSDK",
+});
