9f86a2e47980f4ddbfac61ed7a9afe4a5cb0c748
Rodolphe Stoclin <srodolphe@gmail.com>
2024-09-26 10:06:44 +0200

    ♻️ Rename bundle output file

--- packages/sdk/tsup.config.bundle.ts
+++ packages/sdk/tsup.config.bundle.ts
@@ -5,6 +5,11 @@ export default defineConfig({
     // All of our entry-points
     entry: ["src/bundle.ts"],
     outDir: "dist/bundle",
+    outExtension() {
+        return {
+            js: ".js",
+        };
+    },
     // No external dependencies for these libraries (will be bundled)
     noExternal: ["async-lz-string", "js-sha256", "viem"],
     // Format waited
