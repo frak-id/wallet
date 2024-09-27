52f07e2f5a1144952c675f27348f95bf2ec9ebf0
KONFeature <quentin@nivelais.com>
2024-09-27 20:30:45 +0200

    ✨ Link every pieces together

--- packages/backend-elysia/src/domain/nexus/interactions/routes/purchase.ts
+++ packages/backend-elysia/src/domain/nexus/interactions/routes/purchase.ts
@@ -1,16 +1,23 @@
 import { t } from "@backend-utils";
 import { Elysia } from "elysia";
+import { interactionsPurchaseTrackerTable } from "../../db/interactions";
 import { interactionsContext } from "../context";
 
 export const purchaseInteractionsRoutes = new Elysia()
     .use(interactionsContext)
     .post(
         "/listenForPurchase",
-        async () => {
-            // todo: Store mapping
-            // todo: Precheck current status (if all good directly build and submit stuff)
-            // todo: Otherwise expose endpoint to be called by business part? Or how to do the link between the two cleanly?
-            return "ok";
+        async ({ body, interactionsDb }) => {
+            // todo: Additional security measures, scoped JWT would be ideal
+            // Insert the purchase tracker
+            await interactionsDb
+                .insert(interactionsPurchaseTrackerTable)
+                .values({
+                    wallet: body.wallet,
+                    externalCustomerId: body.customerId,
+                    externalPurchaseId: body.orderId,
+                    token: body.token,
+                });
         },
         {
             isNexusAuthenticated: false,
