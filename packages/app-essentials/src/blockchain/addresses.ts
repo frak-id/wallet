732e4a9aa572f7c0330a3b1b3cc88cd5f0c844ed
KONFeature <quentin@nivelais.com>
2024-09-29 18:00:44 +0200

    🐛 Update addresses

--- packages/app-essentials/src/blockchain/addresses.ts
+++ packages/app-essentials/src/blockchain/addresses.ts
@@ -5,7 +5,7 @@ export const addresses = {
     // nexus
     campaignBankFactory: "0x383922F9ab2Ee7b61Cd2BFF1DEB782733eDc246F",
     campaignFactory: "0xe621E3E82bEAD4a10f2f2e95f6297b659Fc7525D",
-    facetFactory: "0x4B189E97a1ccC40Ae5F0E014603c69e4c80a57a4",
+    facetFactory: "0x69e37f52E1A2Fff5026b212b32207d3E0Cdceb58",
     mUSDToken: "0x56039fa1a804F614eBD714139F29a3ff4DB57ad6",
     productAdministratorRegistry: "0x932fe8a2dd6f8d1009FB916f0F1FEB85cB18316E",
     productInteractionManager: "0x089369aD02bc21459fE4101151fD6D2bBD7FA08A",
@@ -14,7 +14,7 @@ export const addresses = {
     referralRegistry: "0x80B0385e97263A60BafB554B0C4152261D45785A",
     // Kernel
     interactionDelegator: "0x4b8350E6291063bF14ca1E4379147a3bd23714CB",
-    interactionDelegatorAction: "0x47860892a7c31C775a034F8ADEa1c36533C350bc",
+    interactionDelegatorAction: "0xDb814094B3CE4D0DCd4e3B44E584df6d0C265cE5",
     interactionDelegatorValidator: "0xb33cc9Aea3f6e1125179Ec0A1D9783eD3717d04C",
     p256Wrapper: "0x97A24c95E317c44c0694200dd0415dD6F556663D",
     webAuthNRecoveryAction: "0x8b29229515D3e5b829D59617A791b5B3a2c32ff1",
