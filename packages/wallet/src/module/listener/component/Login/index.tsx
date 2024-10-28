3be3a82ebb5ce1ae3a12553fca121d0fc1a723bc
KONFeature <quentin@nivelais.com>
2024-10-28 18:58:20 +0100

    ⚰️ Remove third parties storage stuff

--- packages/wallet/src/module/listener/component/Login/index.tsx
+++ packages/wallet/src/module/listener/component/Login/index.tsx
@@ -1,4 +1,3 @@
-import { authenticatedBackendApi } from "@/context/common/backendClient";
 import type { IFrameResolvingContext } from "@/context/sdk/utils/iFrameRequestResolver";
 import { useConsumePendingSso } from "@/module/authentication/hook/useConsumePendingSso";
 import {
@@ -11,16 +10,12 @@ import { sessionAtom } from "@/module/common/atoms/session";
 import { RequireWebAuthN } from "@/module/common/component/RequireWebAuthN";
 import { modalDisplayedRequestAtom } from "@/module/listener/atoms/modalEvents";
 import styles from "@/module/listener/component/Modal/index.module.css";
-import { getSafeSession } from "@/module/listener/utils/localStorage";
-import { getSharedStorageAccessStatus } from "@/module/listener/utils/thirdParties";
 import type {
     LoginModalStepType,
     SsoMetadata,
 } from "@frak-labs/nexus-sdk/core";
-import { jotaiStore } from "@module/atoms/store";
 import { Spinner } from "@module/component/Spinner";
 import { prefixModalCss } from "@module/utils/prefixModalCss";
-import { useMutation, useQuery } from "@tanstack/react-query";
 import { useAtomValue } from "jotai/index";
 import { type ReactNode, useEffect, useMemo, useState } from "react";
 import { useModalTranslation } from "../../hooks/useModalTranslation";
@@ -52,13 +47,6 @@ export function LoginModalStep({
         // On success, transmit the wallet address up a level
         onSuccess: (session) => onFinish({ wallet: session.address }),
     });
-    const { data: hasStorageAccess } = useQuery({
-        queryKey: ["storage", "access"],
-        queryFn: () => getSharedStorageAccessStatus(),
-    });
-
-    const { mutateAsyncUpdateSessionStatus } = useUpdateSessionStatus();
-
     const session = useAtomValue(sessionAtom);
 
     /**
@@ -92,42 +80,7 @@ export function LoginModalStep({
                         className={`${styles.modalListener__buttonSecondary} ${prefixModalCss("button-secondary")}`}
                         disabled={isLoading}
                         onClick={() => {
-                            // If he already accepted storage access, directly login
-                            if (hasStorageAccess) {
-                                login({});
-                                return;
-                            }
-
-                            // Otherwise, ask for storage, then login if needed only
-                            document
-                                .requestStorageAccess()
-                                .then(getSharedStorageAccessStatus)
-                                .then(async (hasAccess) => {
-                                    // Check for storage access post request
-                                    if (hasAccess) {
-                                        const session =
-                                            await mutateAsyncUpdateSessionStatus();
-                                        if (session) {
-                                            onFinish({
-                                                wallet: session.address,
-                                            });
-                                            return false;
-                                        }
-                                    }
-
-                                    // Tell that we should login here
-                                    return true;
-                                })
-                                .catch(() => {
-                                    // If we failed to get storage access, we need to login
-                                    return true;
-                                })
-                                .then(async (shouldLogin) => {
-                                    if (!shouldLogin) return;
-
-                                    // If we are here, we need to login
-                                    await login({});
-                                });
+                            login({});
                         }}
                     >
                         {isLoading && <Spinner />}
@@ -210,9 +163,7 @@ function SsoButton({
 }
 
 function RegularSsoButton({ link, text }: { link: string; text: ReactNode }) {
-    const { mutateAsyncUpdateSessionStatus } = useUpdateSessionStatus();
     const [failToOpen, setFailToOpen] = useState(false);
-    const [hasClicked, setHasClicked] = useState(false);
 
     // If we failed to open the SSO modal, fallback to a link
     if (failToOpen) {
@@ -237,23 +188,10 @@ function RegularSsoButton({ link, text }: { link: string; text: ReactNode }) {
                 // If we got a window, focus it and save the clicked state
                 if (openedWindow) {
                     openedWindow.focus();
-                    setHasClicked(true);
                 } else {
                     // Otherwise, mark that we fail to open it
                     setFailToOpen(true);
                 }
-            }}
-            onFocus={() => {
-                // If the user didn't clicked the button, do nothing
-                if (!hasClicked) return;
-
-                // On refocus, recheck the storage access status
-                getSharedStorageAccessStatus().then(async (hasAccess) => {
-                    // If we have access, we can update the session status
-                    if (hasAccess) {
-                        await mutateAsyncUpdateSessionStatus();
-                    }
-                });
             }}
         >
             {text}
@@ -265,59 +203,14 @@ function RegularSsoButton({ link, text }: { link: string; text: ReactNode }) {
  * SSO button using a simple link, with sharing stauts
  */
 function LinkSsoButton({ link, text }: { link: string; text: ReactNode }) {
-    const { mutateAsyncUpdateSessionStatus } = useUpdateSessionStatus();
-    const [hasClicked, setHasClicked] = useState(false);
-
     return (
         <a
             href={link}
-            onClick={() => {
-                setHasClicked(true);
-            }}
             className={`${styles.modalListener__buttonPrimary} ${prefixModalCss("button-primary")}`}
             target="frak-sso"
             rel="noreferrer"
-            onFocus={() => {
-                // If the user didn't clicked the button, do nothing
-                if (!hasClicked) return;
-
-                // On refocus, recheck the storage access status
-                getSharedStorageAccessStatus().then(async (hasAccess) => {
-                    // If we have access, we can update the session status
-                    if (hasAccess) {
-                        await mutateAsyncUpdateSessionStatus();
-                    }
-                });
-            }}
         >
             {text}
         </a>
     );
-}
-
-/**
- * This mutation is used to ensure that post SSO we have a session, not automatically updated
- */
-function useUpdateSessionStatus() {
-    const { mutateAsync: mutateAsyncUpdateSessionStatus } = useMutation({
-        mutationKey: ["session", "force-refetch"],
-        mutationFn: async () => {
-            // If our jotai store already contain a session, we can early exit
-            const currentSession = getSafeSession();
-            if (currentSession) {
-                return currentSession;
-            }
-
-            // Otherwise we fetch the session
-            const { data: session } =
-                await authenticatedBackendApi.auth.wallet.session.get();
-            if (session) {
-                jotaiStore.set(sessionAtom, session);
-            }
-
-            return session;
-        },
-    });
-
-    return { mutateAsyncUpdateSessionStatus };
 }
