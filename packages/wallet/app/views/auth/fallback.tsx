ca3f3421a16241c29959119eeca3846561d6938e
KONFeature <quentin@nivelais.com>
2024-12-04 15:39:17 +0100

    🚧 Startup privy as webauthn fallback solution

---
+++ packages/wallet/app/views/auth/fallback.tsx
@@ -0,0 +1,30 @@
+import { Back } from "@/module/common/component/Back";
+import { Grid } from "@/module/common/component/Grid";
+import styles from "@/views/auth/login.module.css";
+import { Button } from "@module/component/Button";
+import { Spinner } from "@module/component/Spinner";
+import { usePrivy } from "@privy-io/react-auth";
+
+export default function Fallback() {
+    const { ready, login } = usePrivy();
+
+    if (!ready) {
+        return <Spinner />;
+    }
+
+    return (
+        <>
+            <Back href={"/register"}>Back to biometric login</Back>
+            <Grid
+                className={styles.login__grid}
+                footer={
+                    <>{/*    todo: More specific CGU/CGV with privy infos?*/}</>
+                }
+            >
+                <Button type={"button"} onClick={() => login()}>
+                    Login via Privy
+                </Button>
+            </Grid>
+        </>
+    );
+}
