53e6fad9b1d995ab7d4e169fc896e4be8e9b8d8d
Rodolphe Stoclin <srodolphe@gmail.com>
2024-12-10 16:07:29 +0100

    🔧 Build examples with SST StaticSite

--- example/news-interactions/app/views/article.tsx
+++ example/news-interactions/app/views/article.tsx
@@ -1,20 +1,9 @@
 import { NewsArticle } from "@/module/news/component/NewsArticle";
-import type { LoaderFunction } from "react-router";
-import { useLoaderData } from "react-router";
-
-export const loader: LoaderFunction = async ({ request }) => {
-    const url = new URL(request.url);
-    const articleId = url.searchParams.get("id");
-
-    if (!articleId) {
-        return { articleId: null };
-    }
-
-    return { articleId };
-};
+import { useSearchParams } from "react-router";
 
 export default function Article() {
-    const { articleId } = useLoaderData<typeof loader>();
+    const [searchParams] = useSearchParams();
+    const articleId = searchParams.get("id");
 
     if (!articleId) {
         // Invalid article
