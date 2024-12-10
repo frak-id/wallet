b312ed2dbbb7e9c9afa3ce025f062737289369fe
Rodolphe Stoclin <srodolphe@gmail.com>
2024-12-10 14:06:34 +0100

    🔥 Remove shopify app
    Copied in https://github.com/frak-id/shopify-app

--- packages/shopify-app/app/types/context.ts
+++
@@ -1,8 +0,0 @@
-import type { authenticate } from "../shopify.server";
-
-/**
- * Simple type for the authenticated admin context
- */
-export type AuthenticatedContext = Awaited<
-    ReturnType<typeof authenticate.admin>
->;
