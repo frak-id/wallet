74400baef04bcc81b7f2cdf5135d3908d6f65ad7
KONFeature <quentin@nivelais.com>
2025-01-14 16:25:32 +0100

    🐛 Update usd -> eur rate

--- packages/dashboard/src/module/common/utils/convertToEuro.ts
+++ packages/dashboard/src/module/common/utils/convertToEuro.ts
@@ -2,11 +2,6 @@ import { formatPrice } from "@/module/common/utils/formatPrice";
 
 export function convertToEuro(price?: string | number) {
     if (typeof price === "undefined") return;
-    const amountInEuros = Number(price) * 0.91; // Assuming 1 USD = 0.91 EUR
-    const formattedEuros = formatPrice(
-        amountInEuros.toString(),
-        undefined,
-        "EUR"
-    );
-    return formattedEuros;
+    const amountInEuros = Number(price) * 0.97; // Assuming 1 USD = 0.97 EUR
+    return formatPrice(amountInEuros.toString(), undefined, "EUR");
 }
