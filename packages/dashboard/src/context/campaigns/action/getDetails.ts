28e597764d355c085b133d35a7b1878dc7c02f94
KONFeature <quentin@nivelais.com>
2025-01-17 11:22:19 +0100

    🐛 Fix getdetails returning non serialisable properties

--- packages/dashboard/src/context/campaigns/action/getDetails.ts
+++ packages/dashboard/src/context/campaigns/action/getDetails.ts
@@ -21,9 +21,19 @@ export async function getCampaignDetails({
     campaignId,
 }: { campaignId: string }) {
     const campaignRepository = await getCampaignRepository();
-    return await campaignRepository.getOneById(
+    const campaignDb = await campaignRepository.getOneById(
         ObjectId.createFromHexString(campaignId)
     );
+    if (!campaignDb) {
+        return null;
+    }
+    return {
+        ...campaignDb,
+        // Remove ObjectId properties
+        _id: undefined,
+        // Send back the id in the id field
+        id: campaignDb._id.toHexString(),
+    };
 }
 
 /**
