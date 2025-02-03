aa1e0eeaba0295420dcd9ccfc093314fb8217ae7
Rodolphe Stoclin <srodolphe@gmail.com>
2025-02-03 18:03:23 +0100

    🐛 Fix Safari <= 15 on news paper, add Object.hasOwn polyfill

--- packages/shared/module/utils/polyfills.ts
+++ packages/shared/module/utils/polyfills.ts
@@ -7,4 +7,12 @@ export async function loadPolyfills() {
             console.error("Failed to load Array.at polyfill:", err);
         }
     }
+    if (typeof Object.hasOwn !== "function") {
+        try {
+            // @ts-ignore
+            await import("core-js/es/object/has-own");
+        } catch (err) {
+            console.error("Failed to load Object.hasOwn polyfill:", err);
+        }
+    }
 }
