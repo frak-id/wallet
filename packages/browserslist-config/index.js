b7f7297546f9f49642049b11bcf0b3e1caddc988
Rodolphe Stoclin <srodolphe@gmail.com>
2025-02-04 10:30:38 +0100

    ✨ Add `@frak-labs/browserslist-config` to share browserlist config

---
+++ packages/browserslist-config/index.js
@@ -0,0 +1,7 @@
+/**
+ * Browserslist configuration used by the monorepo.
+ *
+ * @see https://browsersl.ist/#q=%3E0.2%25%2C+last+2+versions%2C+safari%3E%3D12%2C+not+dead
+ */
+
+module.exports = [">0.2%", "last 2 versions", "safari>=12", "not dead"];
