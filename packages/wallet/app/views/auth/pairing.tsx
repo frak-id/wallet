b0b933fb280c591a4b35ad5207130383e05cb879
Rodolphe Stoclin <srodolphe@gmail.com>
2025-04-04 14:28:18 +0200

    🚧 Implement phone pairing UI

--- packages/wallet/app/views/auth/pairing.tsx
+++ packages/wallet/app/views/auth/pairing.tsx
@@ -1,55 +1,77 @@
 import { Grid } from "@/module/common/component/Grid";
-import { TargetPairingClient } from "@/module/pairing/clients/target";
+import { Title } from "@/module/common/component/Title";
+import { getTargetPairingClient } from "@/module/pairing/clients/store";
+import { PairingHeader } from "@/module/pairing/component/PairingHeader";
+import { PairingInfo } from "@/module/pairing/component/PairingInfo";
+import { Button } from "@shared/module/component/Button";
+import { Spinner } from "@shared/module/component/Spinner";
+import { AlertCircle } from "lucide-react";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useSearchParams } from "react-router";
+import styles from "./pairing.module.css";
 
 /**
  * Pairing page
  * @returns A page to pair with the wallet
  */
 export default function Pairing() {
+    const client = getTargetPairingClient();
     const [searchParams] = useSearchParams();
-    const { t } = useTranslation();
     const code = searchParams.get("code");
+    const { t } = useTranslation();
 
     useEffect(() => {
         if (!code) return;
-
-        const client = new TargetPairingClient();
         client.joinPairing(code);
-    }, [code]);
+    }, [code, client]);
+
+    if (!code) {
+        return (
+            <Grid>
+                <Title size="big" align="center">
+                    {t("wallet.pairing.error.title")}
+                </Title>
+                <p className={styles.pairing__error}>
+                    <AlertCircle size={24} />
+                    {t("wallet.pairing.error.noCode")}
+                </p>
+            </Grid>
+        );
+    }
+
+    if (!client.state.partnerDevice) {
+        return (
+            <Grid>
+                <Title
+                    size="big"
+                    align="center"
+                    className={styles.pairing__loading}
+                >
+                    <Spinner />
+                    {t("wallet.pairing.loading.title")}
+                </Title>
+            </Grid>
+        );
+    }
 
     return (
         <Grid>
-            {code ? (
-                <div>
-                    <h1>
-                        {t("wallet.pairing.title", "Pairing in progress...")}
-                    </h1>
-                    <p>
-                        {t(
-                            "wallet.pairing.processing",
-                            "Processing your pairing request..."
-                        )}
-                    </p>
+            <>
+                <PairingHeader />
+                <PairingInfo client={client} />
+                <div className={styles.pairing__buttons}>
+                    <Button
+                        variant="secondary"
+                        onClick={() => {
+                            client.disconnect();
+                        }}
+                    >
+                        {t("wallet.pairing.cancel")}
+                    </Button>
+                    <Button>{t("wallet.pairing.confirm")}</Button>
                 </div>
-            ) : (
-                <div>
-                    <h1>
-                        {t(
-                            "wallet.pairing.error.title",
-                            "Invalid Pairing Request"
-                        )}
-                    </h1>
-                    <p>
-                        {t(
-                            "wallet.pairing.error.noCode",
-                            "No pairing code provided"
-                        )}
-                    </p>
-                </div>
-            )}
+            </>
         </Grid>
     );
 }
