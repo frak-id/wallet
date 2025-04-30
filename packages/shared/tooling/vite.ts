498f0ff2c62544ff333f22cada844f15d41bc353
KONFeature <quentin@nivelais.com>
2025-04-30 11:07:46 +0200

    🚨 Format vite config

--- packages/shared/tooling/vite.ts
+++ packages/shared/tooling/vite.ts
@@ -1,6 +1,7 @@
 import type { LoggingFunction, ManualChunkMeta, RollupLog } from "rollup";
 
 // Define some specific vendor chunks
+// ffs let's switch to rspack: https://rspack.dev/plugins/webpack/split-chunks-plugin
 const vendorChunks = {
     // Generic UI related chunks
     1: [
@@ -19,13 +20,10 @@ const vendorChunks = {
         "node_modules/ox",
         "node_modules/permissionless",
         "node_modules/@simplewebauthn",
-        "node_modules/@peculiar"
+        "node_modules/@peculiar",
     ],
     // Translation + shared ui
-    3: [
-        "i18next",
-        "packages/shared"
-    ],
+    3: ["i18next", "packages/shared"],
 };
 
 // Define the app packages
@@ -45,7 +43,7 @@ const unhandledPackages = [
 /**
  * Create some manual chunks
  *  -> The check order should be as follow -> unhandled -> listener -> app -> vendorChunks -> vendor
- * 
+ *
  * If no primary match found, check for potentially sub vendor lib (if also used on app or unhandled it will be placed in the general vendor chunk)
  */
 export function manualChunks(id: string, meta: ManualChunkMeta) {
@@ -102,32 +100,38 @@ function findVendorChunk(
     }
 
     // Iterate over each imports
-    const packages = importers.map((importer) => {
-        // If the package is in the unhandled packages, return undefined
-        if (unhandledPackages.find((pkg) => importer.includes(pkg))) {
-            return undefined;
-        }
-
-        // If the root package is in the app packages, return "app"
-        if (appPackages.find((pkg) => importer.includes(pkg))) {
-            return "app";
-        }
-
-        // Try to find a matching vendor chunk
-        for (const [key, libs] of Object.entries(vendorChunks)) {
-            if (libs.find((lib) => importer.includes(lib))) {
-                return `vendor${key}`;
+    const packages = importers
+        .map((importer) => {
+            // If the package is in the unhandled packages, return undefined
+            if (unhandledPackages.find((pkg) => importer.includes(pkg))) {
+                return undefined;
             }
-        }
 
-        // Recursively find the importer package
-        const recursiveImporter = findVendorChunk(importer, meta, (iterations ?? 0) + 1);
-        if (recursiveImporter) {
-            return recursiveImporter;
-        }
+            // If the root package is in the app packages, return "app"
+            if (appPackages.find((pkg) => importer.includes(pkg))) {
+                return "app";
+            }
 
-        return undefined;
-    }).filter(Boolean);
+            // Try to find a matching vendor chunk
+            for (const [key, libs] of Object.entries(vendorChunks)) {
+                if (libs.find((lib) => importer.includes(lib))) {
+                    return `vendor${key}`;
+                }
+            }
+
+            // Recursively find the importer package
+            const recursiveImporter = findVendorChunk(
+                importer,
+                meta,
+                (iterations ?? 0) + 1
+            );
+            if (recursiveImporter) {
+                return recursiveImporter;
+            }
+
+            return undefined;
+        })
+        .filter(Boolean);
 
     if (packages.length === 0) {
         return undefined;
