fc534349eca7bf416c5c468a49f5604fa82932ea
KONFeature <quentin@nivelais.com>
2025-06-25 20:36:34 +0200

    🚑️ Reput interaction listener on elysia

--- services/backend/src/api/wallet/routes/interactions/index.ts
+++ services/backend/src/api/wallet/routes/interactions/index.ts
@@ -1,7 +1,9 @@
 import { Elysia } from "elysia";
+import { purchaseInteractionsRoutes } from "./purchase";
 import { pushInteractionsRoutes } from "./push";
 import { simulateRoutes } from "./simulate";
 
 export const interactionsRoutes = new Elysia({ prefix: "/interactions" })
     .use(pushInteractionsRoutes)
-    .use(simulateRoutes);
+    .use(simulateRoutes)
+    .use(purchaseInteractionsRoutes);
