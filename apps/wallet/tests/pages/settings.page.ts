7a225b6d6718eb032f349b2828a2e0c73e1c9f27
Rubikson <valery@nivelais.com>
2025-07-19 01:21:34 +0200

    ♻️ refactor  settings  page test

--- apps/wallet/tests/pages/settings.page.ts
+++ apps/wallet/tests/pages/settings.page.ts
@@ -22,15 +22,87 @@ export class SettingsPage {
         await this.page.waitForURL("/settings");
     }
 
-    //verify that the settings page is well displayed
-    async verifyDisplaySettingsPage() {
+    //verify the settings button and click it
+    async SettingsButton() {
+        // Ensure the settings button is visible and clickable
+        const settingsLinkLocator = this.page.locator(
+            "a:has(svg.lucide-settings)"
+        );
+        await expect(settingsLinkLocator).toBeVisible();
+    }
+
+    async verifyBiometryInformation() {
+        // the biometry block is visible
         await expect(
             this.page.getByText("Biometry informations")
         ).toBeVisible();
-        await expect(this.page.getByText('Wallet not activated Activate')).toBeVisible();
+    }
+
+    async verifyRecoverySetup() {
+        // Verify the recovery setup section is visible
         await expect(this.page.getByText("Recovery setup")).toBeVisible();
-        await expect(this.page.getByRole('link', { name: 'Setup new recovery' })).toBeVisible();
+        await expect(
+            this.page.getByRole("link", { name: "Setup new recovery" })
+        ).toBeVisible();
+    }
+
+    async verifyLogoutButton() {
+        // Verify the logout button is visible
         await expect(this.page.getByText("Logout")).toBeVisible();
+    }
+
+    async verifyActivateWalletButton() {
+        // Just check checkbox is present, not it's state
+        await expect(this.page.getByText("Wallet not activated")).toBeVisible();
+    }
+
+    async verifyNotificationsButton() {
+        await expect(
+            this.page.getByRole("link", { name: "Notifications" })
+        ).toBeVisible();
+    }
+    
+    async verifyNotificationsButtonClick() {
+        const notificationsButton = this.page.getByRole("link", {
+            name: "Notifications",
+        });
+        await expect(notificationsButton).toBeVisible();
+        await notificationsButton.click();
+        await this.page.waitForURL("/notifications");
+    }
+
+    async verifyUnsubscribeNotifications() {
+        await expect(this.page.getByText("unsubscribe")).toBeVisible();
+    }
+
+    async verifyPairedWallets() {
+        // todo: for later
+    }
+
+    // chek the authenticator button is visible
+    async verifyAuthenticatorButton() {
+        const authenticatorButton = this.page.getByText(
+            /Authenticator: [A-Za-z0-9.]+$/i
+        );
+        await expect(authenticatorButton).toBeVisible();
+    }
+
+    // copy the authenticator button text
+    async copyAuthenticatorInformations() {
+        const authenticatorButton = this.page.getByText(
+            /Authenticator: [A-Za-z0-9.]+$/i
+        );
+        await expect(authenticatorButton).toBeVisible();
+        // click the authenticator button and copy the text
+        await expect(authenticatorButton).toBeEnabled();
+        await authenticatorButton.click();
+    }
+
+    async verifyWalletButton() {
+        // Verify that the "wallet:"" button is visible
+        await expect(
+            this.page.getByText(/Wallet: [A-Za-z0-9.]+$/i)
+        ).toBeVisible();
     }
 
     /**
@@ -46,7 +118,6 @@ export class SettingsPage {
     // verify the activate wallet button
     async clickActivateWalletButton() {
         await this.page.getByRole("switch").click();
-
         //verify the text after clicking activate wallet button
         await expect(
             this.page.getByText("Your wallet is activated")
@@ -56,37 +127,9 @@ export class SettingsPage {
     // verify the desactivate wallet button
     async clickDesactivateWalletButton() {
         await this.page.getByRole("switch").click();
-
         //verify the text after clicking desactivate wallet button
         await expect(
             this.page.getByText("Your wallet is non activated")
-        ).toBeVisible();
-    }
-
-    // chek the authenticator button is visible
-    async authenticatorButtonVisible() {
-        const authenticatorButton = this.page.getByText(
-            "Authenticator: 0x5545...565051"
-        );
-        await expect(authenticatorButton).toBeVisible();
-    }
-
-    // copy the authenticator button text
-    async copyAuthenticatorInformations() {
-        const authenticatorButton = this.page
-            .locator("button._walletAddress_1ys0n_1")
-            .nth(0);
-        await expect(authenticatorButton).toBeVisible();
-
-        // click the authenticator button and copy the text
-        await expect(authenticatorButton).toBeEnabled();
-        await authenticatorButton.click();
-    }
-
-    async verifyWalletButton() {
-        // Verify that the "wallet:"" button is visible
-        await expect(
-            this.page.getByText(/Wallet: [A-Za-z0-9.]+$/i)
         ).toBeVisible();
     }
 }
--- apps/wallet/tests/specs/settings/on-device.spec.ts
+++ apps/wallet/tests/specs/settings/on-device.spec.ts
@@ -7,51 +7,41 @@ test.beforeEach(async ({ mockedWebAuthN }) => {
 //verify display the settings page
 test("should display settings", async ({ settingsPage }) => {
     await settingsPage.navigateToSettings();
-    await settingsPage.verifyDisplaySettingsPage();
-});
-
-//verify click recovery button
-test("should click recovery button", async ({ settingsPage }) => {
-    await settingsPage.navigateToSettings();
-    await settingsPage.verifyDisplaySettingsPage();
-    await settingsPage.clickRecoveryButton();
-});
-
-//verify the activate wallet button
-// todo: mock the activation / deactivation of the wallet
-test.skip("click activate wallet button", async ({ settingsPage }) => {
-    await settingsPage.navigateToSettings();
-    await settingsPage.verifyDisplaySettingsPage();
-    await settingsPage.clickActivateWalletButton();
-});
-
-//verify the desactivate wallet button
-// todo: mock the activation / deactivation of the wallet
-test.skip("click desactivate wallet button", async ({ settingsPage }) => {
-    await settingsPage.navigateToSettings();
-    await settingsPage.verifyDisplaySettingsPage();
-    await settingsPage.clickDesactivateWalletButton();
-    // need to be locked at the wallet to verify the desactivate wallet button
 });
 
 // verify the "wallet:" button is visible
 test("wallet button visible", async ({ settingsPage }) => {
     // Navigate to the settings page
     await settingsPage.navigateToSettings();
-    // Verify that the history page is displayed with rewards and interactions
-    await settingsPage.verifyDisplaySettingsPage();
     // Verify that the "wallet:" button is visible
     await settingsPage.verifyWalletButton();
+});
+
+//verify logout button visible
+test("logout  button is visble ", async ({ settingsPage }) => {
+    await settingsPage.navigateToSettings();
+    await settingsPage.verifyLogoutButton();
+});
+
+//verify the setting button is visible
+test("setting button visible", async ({ settingsPage }) => {
+    await settingsPage.navigateToSettings();
+    await settingsPage.SettingsButton();
+});
+
+//verify biometry block is viisble
+test("should display biometry informations", async ({ settingsPage }) => {
+      // Navigate to the settings page
+    await settingsPage.navigateToSettings();
+    await settingsPage.verifyBiometryInformation();
 });
 
 // verify the authenticator button is visible
 test("authenticator button visible", async ({ settingsPage }) => {
     // Navigate to the settings page
     await settingsPage.navigateToSettings();
-    // Verify that the history page is displayed with rewards and interactions
-    await settingsPage.verifyDisplaySettingsPage();
     // Verify that the authenticator button is visible
-    await settingsPage.authenticatorButtonVisible();
+    await settingsPage.verifyAuthenticatorButton();
 });
 
 // verify copy authenticator information in clipboard
@@ -61,10 +51,52 @@ test("should copy authenticator information", async ({
 }) => {
     // Navigate to the settings page
     await settingsPage.navigateToSettings();
-    // Verify that the history page is displayed with rewards and interactions
-    await settingsPage.verifyDisplaySettingsPage();
     // Copy authenticator information
     await settingsPage.copyAuthenticatorInformations();
     // Verify that the clipboard is not empty
     await clipboardHelper.verifyClipboardNotEmpty();
+});
+
+//verify click recovery button
+test("should click recovery button", async ({ settingsPage }) => {
+    await settingsPage.navigateToSettings();
+    await settingsPage.clickRecoveryButton();
+});
+
+//verify the activate wallet button
+// todo: mock the activation / deactivation of the wallet
+test.skip("click activate wallet button", async ({ settingsPage }) => {
+    await settingsPage.navigateToSettings();
+    await settingsPage.clickActivateWalletButton();
+});
+
+//verify the desactivate wallet button
+// todo: mock the activation / deactivation of the wallet
+test.skip("click desactivate wallet button", async ({ settingsPage }) => {
+    await settingsPage.navigateToSettings();
+    await settingsPage.clickDesactivateWalletButton();
+    // need to be locked at the wallet to verify the desactivate wallet button
+});
+
+//verify the notification  button is visible
+test("notification  button is visble ", async ({ settingsPage }) => {
+    await settingsPage.navigateToSettings();
+    await settingsPage.verifyNotificationsButton();
+});
+
+//verify the notifcation button click and notifications page
+test("notification  button click ", async ({ settingsPage }) => {
+    await settingsPage.navigateToSettings();
+    await settingsPage.verifyNotificationsButtonClick();
+});
+
+//verify the unsubscribe notifications
+test.skip("unsubscribe notifications block", async ({ settingsPage }) => {
+    await settingsPage.verifyUnsubscribeNotifications();
+});
+
+//verify settings button click
+test("setting button click", async ({ settingsPage }) => {
+    await settingsPage.navigateToSettings();
+    await settingsPage.clickSettingsButton();
 });
