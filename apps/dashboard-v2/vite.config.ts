37105a20eed238d209c7f1caee1ddd6f06594db1
KONFeature <quentin@nivelais.com>
2025-10-30 12:54:26 +0100

    🔧 Add business env variable

--- apps/dashboard-v2/vite.config.ts
+++ apps/dashboard-v2/vite.config.ts
@@ -17,4 +17,24 @@ export default defineConfig({
             },
         }),
     ],
+    // Replace some env variable when it's needed
+    define: {
+        "process.env.STAGE": JSON.stringify(process.env.STAGE),
+        "process.env.FRAK_WALLET_URL": JSON.stringify(process.env.FRAK_WALLET_URL),
+        "process.env.BACKEND_URL": JSON.stringify(process.env.BACKEND_URL),
+        "process.env.INDEXER_URL": JSON.stringify(process.env.INDEXER_URL),
+        "process.env.ERPC_URL": JSON.stringify(process.env.ERPC_URL),
+        "process.env.OPEN_PANEL_API_URL": JSON.stringify(
+            process.env.OPEN_PANEL_API_URL
+        ),
+        "process.env.OPEN_PANEL_BUSINESS_CLIENT_ID": JSON.stringify(
+            process.env.OPEN_PANEL_BUSINESS_CLIENT_ID
+        ),
+        "process.env.DRPC_API_KEY": JSON.stringify(process.env.DRPC_API_KEY),
+        "process.env.NEXUS_RPC_SECRET": JSON.stringify(process.env.NEXUS_RPC_SECRET),
+        "process.env.FUNDING_ON_RAMP_URL": JSON.stringify(
+            process.env.FUNDING_ON_RAMP_URL
+        ),
+        // Not placing mongo or session encryption key, that's only server side normally
+    },
 });
