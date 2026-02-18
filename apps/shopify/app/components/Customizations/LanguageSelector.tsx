22e3f680663a71515e7a4d2263ebf64d120e337c
Rodolphe Stoclin <srodolphe@gmail.com>
2026-02-18 16:16:50 +0100

    🐛 Fix Polaris s-choice-list: add name/label, use s-choice

--- apps/shopify/app/components/Customizations/LanguageSelector.tsx
+++ apps/shopify/app/components/Customizations/LanguageSelector.tsx
@@ -18,39 +18,19 @@ export function LanguageModeSelector({
             <s-stack gap="base">
                 <s-heading>{t("customizations.languageMode.title")}</s-heading>
                 <s-choice-list
+                    name="languageMode"
+                    label={t("customizations.languageMode.title")}
                     values={[mode]}
                     onChange={(e) =>
                         onModeChange(e.currentTarget.values[0] as LanguageMode)
                     }
                 >
-                    <s-option value="single">
-                        <div>
-                            <div>
-                                {t("customizations.languageMode.single.label")}
-                            </div>
-                            <div
-                                style={{ fontSize: "0.875rem", color: "#666" }}
-                            >
-                                {t(
-                                    "customizations.languageMode.single.helpText"
-                                )}
-                            </div>
-                        </div>
-                    </s-option>
-                    <s-option value="multi">
-                        <div>
-                            <div>
-                                {t("customizations.languageMode.multi.label")}
-                            </div>
-                            <div
-                                style={{ fontSize: "0.875rem", color: "#666" }}
-                            >
-                                {t(
-                                    "customizations.languageMode.multi.helpText"
-                                )}
-                            </div>
-                        </div>
-                    </s-option>
+                    <s-choice value="single">
+                        {t("customizations.languageMode.single.label")}
+                    </s-choice>
+                    <s-choice value="multi">
+                        {t("customizations.languageMode.multi.label")}
+                    </s-choice>
                 </s-choice-list>
             </s-stack>
         </s-section>
