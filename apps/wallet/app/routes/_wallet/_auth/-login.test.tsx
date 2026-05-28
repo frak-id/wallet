9af7960f1f5826f7465b72bb6d70f1237a64043a
Rodolphe Stoclin <srodolphe@gmail.com>
2026-05-28 11:45:47 +0200

    🐛 Fix stale login route import in wallet login test

--- apps/wallet/app/routes/_wallet/_auth/-login.test.tsx
+++ apps/wallet/app/routes/_wallet/_auth/-login.test.tsx
@@ -84,7 +84,7 @@ vi.mock("react-i18next", () => ({
     Trans: ({ i18nKey }: { i18nKey: string }) => i18nKey,
 }));
 
-import { Route } from "./login";
+import { Route } from "./login.index";
 
 const LoginPage = Route.options.component!;
 
